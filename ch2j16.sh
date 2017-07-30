01ee40838d42abdbad1bd3e0fbff87e8d28aa460
Justin Chu <justinchuch@users.noreply.github.com>
2017-07-29 22:45:32 -0400

    Updated: ch2j*.sh - removed export PATH; Beautify bash_profile

--- ch2j16.sh
+++ ch2j16.sh
@@ -4,5 +4,4 @@ echo Please run using: source ch2j16.sh
 /usr/libexec/java_home -V
 export JAVA_HOME=`/usr/libexec/java_home -v 1.6.0_65-b14-468`
 echo "JAVA_HOME: " $JAVA_HOME
-export PATH=${JAVA_HOME}/bin:${PATH}
 java -version
--- ch2j17.sh
+++ ch2j17.sh
@@ -4,5 +4,4 @@ echo Please run using: source ch2j17.sh
 /usr/libexec/java_home -V
 export JAVA_HOME=`/usr/libexec/java_home -v 1.7*`
 echo "JAVA_HOME: " $JAVA_HOME
-export PATH=${JAVA_HOME}/bin:${PATH}
 java -version
